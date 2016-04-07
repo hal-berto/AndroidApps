2f5bb465c41f3583fceb5eba83f6b7c5dc6f0260
alberto.pagani <alberto.pagani@buongiorno.com>
2016-04-07 16:06:51 +0200

    .

--- LibroDelleRisposte/app/src/main/java/librorisposte/android/app/util/ReadResponse.java
+++ LibroDelleRisposte/app/src/main/java/librorisposte/android/app/util/ReadResponse.java
@@ -1,7 +1,10 @@
 package librorisposte.android.app.util;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -11,17 +14,31 @@ public class ReadResponse {
         String response = "";
         try {
             Document doc = Jsoup.connect("http://www.bryonia.it/accessibile/risposte.php").get();
-            response = doc.title();
+            String textToParse = doc.select("p").html();
+
+            if(StringUtils.isBlank(textToParse)){
+                return response;
+            }
+
+            String[] splitted = textToParse.split("(<br>)");
+            for(int i = 1; i < splitted.length; i++){
+                if(StringUtils.isNotBlank(splitted[i])){
+                    response = splitted[i].trim();
+                    break;
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return response;
     }
 
     public static void main(String[] args){
         ReadResponse rr = new ReadResponse();
-        System.out.println(rr.readNewResponse());
+        for(int i = 0; i < 100; i++) {
+            System.out.println(rr.readNewResponse());
+        }
     }
 
 }
