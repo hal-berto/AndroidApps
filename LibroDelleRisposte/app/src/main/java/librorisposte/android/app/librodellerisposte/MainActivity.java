a299d2ec396c08e38aa6ed413d6264f9d39e7a67
alberto.pagani <alberto.pagani@buongiorno.com>
2016-04-11 14:38:09 +0200

    Activities implementation

--- LibroDelleRisposte/app/src/main/java/librorisposte/android/app/librodellerisposte/MainActivity.java
+++ LibroDelleRisposte/app/src/main/java/librorisposte/android/app/librodellerisposte/MainActivity.java
@@ -1,5 +1,6 @@
 package librorisposte.android.app.librodellerisposte;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import librorisposte.android.app.util.ReadResponse;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String RESPONSE_MESSAGE = "librorisposte.android.app.librodellerisposte.RESPONSE_MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +23,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
@@ -48,5 +53,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void queryBook(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayResponseActivity.class);
+        ReadResponse readResponse = new ReadResponse();
+        String message = readResponse.readNewResponse();
+        intent.putExtra(RESPONSE_MESSAGE, message);
+        startActivity(intent);
     }
 }
